dfa831cc2b0763b50ce41fcdb248c0bf87bd81b4
thisiskusal <thisiskusal@gmail.com>
2018-06-23 17:47:40 -0400

    With objects

--- helloWorld/helloWorld.js
+++ helloWorld/helloWorld.js
@@ -1,1 +1,17 @@
-alert("Hello World!");
+"use strict";
+let message = "Hello World!";
+message = 'Hello, World!';
+
+alert('\'' + message
+  + '\' is a '
+  + typeof(message));
+
+let inf = Infinity;
+alert('\'' + inf
+  + '\' is a '
+  + typeof(inf));
+
+let obj = []
+alert('\'' + obj
+  + '\' is a '
+  + typeof(obj));
